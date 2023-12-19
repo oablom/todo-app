9897cfcdd56bb93c36006e8a9870880a4bd793c9
oablom <oablom@gmail.com>
2023-12-19 14:55:30 +0100

    added some functionality to NewTask and Tasks

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -6,10 +6,31 @@
 // Typ av ärende (ha minst tre olika typer av ärenden som man kan välja emellan t.ex hushållssysslor, aktivitet med vänner och jobbrelaterad ärende.
 // Användaren ska också kunna få förslag på en aktivitet att göra genom ett knapptryck. Aktiviteten ska hämtas från https://www.boredapi.com/ - När en aktivitet hämtas ska den fylla Titel-inputen med aktivitetens namn.
 
+import { useState, useEffect } from "react";
+
 export default function NewTask() {
+  let [task, setTask] = useState();
+  let [taskArray, setTaskArray] = useState([]);
+
+  useEffect(() => {
+    async function fetchTask() {
+      try {
+        const response = await fetch("https://www.boredapi.com/api/activity");
+        const data = await response.json();
+        if (response) {
+          setTask(data);
+          console.log(data);
+        }
+      } catch (error) {
+        console.log("Error fetching data (message): ", error);
+      }
+    }
+    fetchTask();
+  }, []);
+
   return (
-    <div>
-      <h1>NewTask</h1>
+    <div className="new-task">
+      <h1>NewTask Component</h1>
     </div>
   );
 }
