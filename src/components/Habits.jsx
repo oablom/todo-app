33c33bf4ef47139a1665ebc39cabe47c92f7ed18
Inclusit <camillefranzen@gmail.com>
2023-12-20 13:18:40 +0100

    Added filter, can't decide between two input styles

--- src/components/Habits.jsx
+++ src/components/Habits.jsx
@@ -34,26 +34,59 @@ export default function Habits() {
     priority: "low",
     streak: 11}]);
 
+  const [priorityFilter, setPriorityFilter] = useState("all");
+  const [sortedHabits, setSortedHabits] = useState(null);
+
+
   let editHabit = (i, edit) => {
     setHabits(oldHabits => {
       let editedHabits = [...oldHabits];
       edit(editedHabits[i]);
       return editedHabits;
-    }) 
-  }
+    }); 
+  };
+
+ let filteredHabits = () => (
+  priorityFilter === "all" ? habits : habits.filter((habit) => habit.priority === priorityFilter)
+
+ );
+
+ let sortingHabits = () => {
+  sortedHabits === "ascending" ? setSortedHabits(habits.sort((high,low) => low.priority.localeCompare(high.priority))) : sortedHabits === "descending" ? setSortedHabits(habits.sort((high,low) => high.priority.localeCompare(low.priority))) : setSortedHabits(null);
   
+ };
+ 
   return (
     <div>
       <h1>Habits</h1>
-      <button>Filter</button>
-      {habits.map((habit, i) => (
+      {/* FILTERED HABITS - FILTRERADE HABITS */}
+      
+      <select value={priorityFilter} onChange={(e) => setPriorityFilter(e.target.value)}>
+        <option value="all">Filter by priority:</option>
+        <option value="all">All</option>
+        <option value="high">High</option>
+        <option value="mid">Medium</option>
+        <option value="low">Low</option>
+      </select>
+
+      {/* <label><input type="radio" value="all" checked={priorityFilter === "all"} onChange={(e) => setPriorityFilter(e.target.value)} />All</label>
+      <label><input type="radio" value="low" checked={priorityFilter === "low"} onChange={(e) => setPriorityFilter(e.target.value)} />Low</label>
+      <label><input type="radio" value="mid" checked={priorityFilter === "mid"} onChange={(e) => setPriorityFilter(e.target.value)} />Medium</label>
+      <label><input type="radio" value="high" checked={priorityFilter === "high"} onChange={(e) => setPriorityFilter(e.target.value)} />High</label> */}
+
+      {/* SORTERADE HABITS */}
+      <label><input type="radio" value="ascending" checked={sortedHabits === "ascending"} onChange={(e) => setSortedHabits(e.target.value)} />Ascending</label>
+      <label><input type="radio" value="descending" checked={sortedHabits === "descending"} onChange={(e) => setSortedHabits(e.target.value)} />Descending</label>
+      
+      {filteredHabits().map((habit, i) => (
         <div  key={i} style={{backgroundColor: habit.priority === "low" ? "green" : habit.priority === "mid" ? "yellow" : "red"}}>
           <p>{habit.title}</p>
-          <p><button onClick={() => editHabit(i, (edit) => habit.streak -= 1)}>sub</button>{habit.streak}<button onClick={() => editHabit(i,(edit) => habit.streak += 1)}>add</button></p>
-          <button onClick={() => editHabit(i, (edit) => habit.streak = 0)}>Reset</button>
+          <p><button onClick={() => editHabit(i, edit => habit.streak -= 1)}>sub</button>{habit.streak}<button onClick={() => editHabit(i,edit => habit.streak += 1)}>add</button></p>
+          <button onClick={() => editHabit(i, edit => habit.streak = 0)}>Reset</button>
           </div>
       ))};
       
     </div>
   );
+  
 };
