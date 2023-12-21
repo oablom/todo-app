878ec9442b286edb23b7a4afea45b6d44c3b0dfb
oablom <oablom@gmail.com>
2023-12-21 14:31:26 +0100

    commit

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,36 +1,120 @@
-// Här ska användaren kunna skapa ett nytt ärende.
-// Varje ärende ska innehålla följande
-// Titel
-// Beskrivning
-// Tidsestimat (hur lång tid tar det att utföra ärendet)
-// Typ av ärende (ha minst tre olika typer av ärenden som man kan välja emellan t.ex hushållssysslor, aktivitet med vänner och jobbrelaterad ärende.
-// Användaren ska också kunna få förslag på en aktivitet att göra genom ett knapptryck. Aktiviteten ska hämtas från https://www.boredapi.com/ - När en aktivitet hämtas ska den fylla Titel-inputen med aktivitetens namn.
-
 import { useState, useEffect } from "react";
 
-export default function NewTask() {
-  let [task, setTask] = useState();
-  let [taskArray, setTaskArray] = useState([]);
+export default function NewTask(props) {
+  const [fetchedData, setFetchedData] = useState({});
+  const [randomise, setRandomise] = useState(false);
+  // const [taskArray, setTaskArray] = useState([]);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [timeEstimate, setTimeEstimate] = useState("");
+  const [type, setType] = useState("");
 
   useEffect(() => {
     async function fetchTask() {
       try {
         const response = await fetch("https://www.boredapi.com/api/activity");
-        const data = await response.json();
-        if (response) {
-          setTask(data);
-          console.log(data);
+
+        if (response.ok) {
+          const data = await response.json();
+          setFetchedData(data);
+          setTitle(fetchedData.activity);
+        } else {
+          setFetchedData("Error fetching data: ", response.status);
         }
       } catch (error) {
         console.log("Error fetching data (message): ", error);
       }
     }
     fetchTask();
-  }, []);
+  }, [randomise]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTask = {
+      title,
+      description,
+      timeEstimate,
+      type,
+    };
+
+    props.taskArrayFunction(newTask);
+    setTitle("");
+    setDescription("");
+    setTimeEstimate("");
+    setType("");
+    taskForm.reset();
+  }
+  const taskForm = document.getElementById("task-form");
+  const inputTitle = document.getElementById("newtask-title");
 
   return (
     <div className="new-task">
       <h1>NewTask Component</h1>
+      <form className="task-form" id="task-form" onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          required
+          type="text"
+          id="newtask-title"
+          name="title"
+          // value={randomise ? fetchedData.activity : ""}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <br />
+        <label htmlFor="description">Description:</label>
+        <input
+          required
+          type="text"
+          id="newtask-description"
+          name="description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <br />
+        <label htmlFor="timeEstimate">Time Estimate:</label>
+        <input
+          required
+          type="number"
+          id="newtask-timeEstimate"
+          name="timeEstimate"
+          onChange={(e) => setTimeEstimate(e.target.value)}
+        />
+        <br />
+        <label htmlFor="type">Type:</label>
+        <select
+          required
+          name="newtask-type"
+          id="type"
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="">Choose task type</option>
+          <option>Work related</option>
+          <option>Hobby</option>
+          <option>Sports</option>
+        </select>
+        <br />
+        <div className="form-buttons">
+          <button type="submit">Add Task</button>
+          <button
+            type="button"
+            onClick={() => {
+              setRandomise(!randomise);
+              inputTitle.value = fetchedData.activity;
+            }}
+          >
+            Randomize Activity
+          </button>
+          <button
+            onClick={() => {
+              taskForm.reset();
+            }}
+          >
+            Clear form
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
